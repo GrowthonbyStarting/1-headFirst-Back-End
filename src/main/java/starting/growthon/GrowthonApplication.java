a29bfd56cce3799608be2df643e723369f5d2472
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2023-06-14 22:53:07 +0900

    🎉 initial commit

---
+++ src/main/java/starting/growthon/GrowthonApplication.java
@@ -0,0 +1,13 @@
+package starting.growthon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowthonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowthonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/starting/growthon/GrowthonApplicationTests.java
@@ -0,0 +1,13 @@
+package starting.growthon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowthonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
