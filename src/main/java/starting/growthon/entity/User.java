19e075cf098d6ebc858d8eb310ea07ff148880ce
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2023-06-18 02:09:51 +0900

    [feat] 유저 role 컬럼 추가
    - 멘토, 멘티 구분 위한 role 컬럼 추가

--- src/main/java/starting/growthon/entity/User.java
+++ src/main/java/starting/growthon/entity/User.java
@@ -1,10 +1,7 @@
 package starting.growthon.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -23,8 +20,10 @@ public class User {
     private String name;
     private Long uuid; // 카카오 고유 ID 위함
 
+    private String role;
     public User(String name, Long uuid) {
         this.name = name;
         this.uuid = uuid;
+        this.role = "MENTEE";
     }
 }
