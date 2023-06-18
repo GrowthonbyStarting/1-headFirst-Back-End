06f70debb204cff30d3a5bbe78825788bdf28345
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2023-06-18 14:54:12 +0900

    [feat] MentorInfo 테이블 생성
    - 멘토 시 정보를 나타내는 MentorInfo 테이블 생성 (User와 1:1 관계로 설정)

--- src/main/java/starting/growthon/entity/User.java
+++ src/main/java/starting/growthon/entity/User.java
@@ -21,6 +21,11 @@ public class User {
     private Long uuid; // 카카오 고유 ID 위함
 
     private String role;
+
+    @OneToOne
+    @JoinColumn(name = "mentor_info_id")
+    private MentorInfo mentorInfo;
+
     public User(String name, Long uuid) {
         this.name = name;
         this.uuid = uuid;
