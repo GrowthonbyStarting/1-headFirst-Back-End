7c4706c7578bd684dd3b54b0b5803a82eff938eb
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2023-06-28 22:05:22 +0900

    Update WebConfig.java

--- src/main/java/starting/growthon/config/WebConfig.java
+++ src/main/java/starting/growthon/config/WebConfig.java
@@ -3,12 +3,15 @@ package starting.growthon.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     private final CorsProperties corsProperties;
+
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
 
     @Autowired
     public WebConfig(CorsProperties corsConfiguration) {
@@ -21,16 +24,19 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins(corsProperties.getOrigins().toArray(new String[0]))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .exposedHeaders("Access-Control-Allow-Origin");
+                .exposedHeaders("Access-Control-Allow-Origin")
+                .allowCredentials(true);
         registry.addMapping("/auth/**")
                 .allowedOrigins(corsProperties.getOrigins().toArray(new String[0]))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .exposedHeaders("Access-Control-Allow-Origin");
+                .exposedHeaders("Access-Control-Allow-Origin")
+                .allowCredentials(true);
         registry.addMapping("/mentor/**")
                 .allowedOrigins(corsProperties.getOrigins().toArray(new String[0]))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .exposedHeaders("Access-Control-Allow-Origin");
+                .exposedHeaders("Access-Control-Allow-Origin")
+                .allowCredentials(true);
     }
 }
