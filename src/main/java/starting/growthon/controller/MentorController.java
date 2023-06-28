d928b7a295c97e290320803d4d30277a8a86d8ab
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2023-06-28 22:20:20 +0900

    Update MentorController.java

--- src/main/java/starting/growthon/controller/MentorController.java
+++ src/main/java/starting/growthon/controller/MentorController.java
@@ -1,6 +1,7 @@
 package starting.growthon.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,9 @@ import starting.growthon.entity.File;
 import starting.growthon.exception.ExceptionResponse;
 import starting.growthon.service.FileService;
 import starting.growthon.service.MentorService;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @CrossOrigin(originPatterns = {"http://localhost:3000", "http://43.201.17.248:3000"})
@@ -36,6 +40,11 @@ public class MentorController {
             MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> profileUpload(@RequestParam(value = "image")
                                            MultipartFile file, File image) throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("text","xml", StandardCharsets.UTF_8));
+        headers.set("Access-Control-Allow-Origin", "*");
+        headers.set("Access-Control-Allow-Methods","GET,POST,OPTIONS,DELETE,PUT");
+        
         try {
             return ResponseEntity.ok(fileService.profileUpload(file, image));
         } catch (IllegalStateException e) {
@@ -47,6 +56,11 @@ public class MentorController {
             MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> resumeUpload(@RequestParam(value = "resume")
                                            MultipartFile file, File resume) throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("text","xml", StandardCharsets.UTF_8));
+        headers.set("Access-Control-Allow-Origin", "*");
+        headers.set("Access-Control-Allow-Methods","GET,POST,OPTIONS,DELETE,PUT");
+
         try {
             return ResponseEntity.ok(fileService.mentorResumeUpload(file, resume));
         } catch (IllegalStateException e) {
