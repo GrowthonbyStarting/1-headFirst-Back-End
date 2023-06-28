7404d812c19fb2b94715c437c8fb549201467f8b
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2023-06-28 22:11:52 +0900

    Update WebConfig.java

--- src/main/java/starting/growthon/config/WebConfig.java
+++ src/main/java/starting/growthon/config/WebConfig.java
@@ -20,6 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        /*
         registry.addMapping("/**")
                 .allowedOrigins(corsProperties.getOrigins().toArray(new String[0]))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
@@ -37,6 +38,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Access-Control-Allow-Origin")
+                .allowCredentials(true);
+         */
+        // 참고: https://velog.io/@wnguswn7/CORS-%EC%97%90%EB%9F%AC-Access-to-XMLHttpRequest-at-from-origin-has-been-blocked-by-CORS-policy
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*") // 안에 해당 주소를 넣어도 됨
+                .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
                 .allowCredentials(true);
     }
 }
